c9d8254dbb9622b82edb44c42f793bcdf64fa5f1
MATTHEW CLARK <matt@notch8.com>
2018-03-16 11:08:50 -0700

    Login page, and Authentication service in frontend

--- auth-frontend/src/index.js
+++ auth-frontend/src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './Login'
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route 
+        exact
+        path='/' 
+        component={App} />
+      <Route 
+        exact 
+        path="/login" 
+        component={Login} />
+    </div>
+  </Router>
+, document.getElementById('root'));
 registerServiceWorker();
